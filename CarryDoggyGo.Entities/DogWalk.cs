8f863adeed3470d80e5175d3a2a50d9f0ddb841c
gsinuiri-coder <gsinuiri@gmail.com>
2021-06-02 18:39:41 -0500

    add qualification

--- CarryDoggyGo.Entities/DogWalk.cs
+++ CarryDoggyGo.Entities/DogWalk.cs
@@ -13,14 +13,15 @@ namespace CarryDoggyGo.Entities
         public string AditionalInformation { get; set; }
         public string PaymentAmount { get; set; }
         public int Calification { get; set; }
+
         public int DogWalkerId { get; set; }
         public virtual DogWalker DogWalker { get; set; }
         public int DogOwnerId { get; set; }
-
         public virtual DogOwner DogOwner { get; set; }
+
+        public virtual Qualification Qualification { get; set; }
+
         public DateTime Date { get; set; }
         public string Address { get; set; }
-       
-          
     }
 }
---
+++ CarryDoggyGo/Models/Qualification/CreateQualification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarryDoggyGo.Models.Qualification
+{
+    public class CreateQualification
+    {
+        public int Score { get; set; }
+        public string Reason { get; set; }
+        public string Recommendation { get; set; }
+    }
+}
