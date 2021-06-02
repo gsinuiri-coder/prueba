ffc352dcdb5b0ea725fb8cd2af20905a4b282fcf
gsinuiri-coder <gsinuiri@gmail.com>
2021-06-02 18:47:49 -0500

    add report entity

--- CarryDoggyGo.Data/DbContextCarryDoggyGo.cs
+++ CarryDoggyGo.Data/DbContextCarryDoggyGo.cs
@@ -19,6 +19,8 @@ namespace CarryDoggyGo.Data
         public DbSet<Dog> Dogs{ get; set; }
         public DbSet<DogCareItem> DogCareItems{ get; set; }
         public DbSet<CareItem> CareItems{ get; set; }
+        public DbSet<Qualification> Qualifications { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -28,8 +30,7 @@ namespace CarryDoggyGo.Data
             modelBuilder.ApplyConfiguration(new DogCareItemMap());
             modelBuilder.ApplyConfiguration(new DogMap());
             modelBuilder.ApplyConfiguration(new CareItemMap());
-
+            modelBuilder.ApplyConfiguration(new QualificationMap());
         }
-
     }
 }
